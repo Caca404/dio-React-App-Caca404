09d45d29b8dbbeee0ada9e9932e49e9922a714c6
Caca404 <christia10k@hotmail.com>
2022-03-19 13:12:23 -0300

    Versão estável do React + Babel + Webpack

--- src/index.js
+++ src/index.js
@@ -1,19 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+// import App from './App.js';
+
+// import './index.css';
 
 const App = () => {
-  return (
-    <div className="App">
-      Hello
-    </div>
-  )
+  return <h1>Hello World from React</h1>
 }
 
-const rootElement = document.getElementById('root')
-ReactDOM.render(<App />, rootElement);
+// const rootElement = React.createElement('h1', null, 'Hello World');
+ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
